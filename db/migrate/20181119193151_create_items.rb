814e5e9cfd5b5bfa4b327e37f37a1a2a91dd7de3
EC2 Default User <ec2-user@ip-172-31-39-103.us-east-2.compute.internal>
2018-11-19 20:00:46 +0000

    Created models and added bootstrap

---
+++ db/migrate/20181119193151_create_items.rb
@@ -0,0 +1,14 @@
+class CreateItems < ActiveRecord::Migration[5.2]
+  def change
+    create_table :items do |t|
+      t.string :iname
+      t.text :description
+      t.int :stock
+      t.string :image
+      t.decimal :price
+      t.string :category
+
+      t.timestamps
+    end
+  end
+end
