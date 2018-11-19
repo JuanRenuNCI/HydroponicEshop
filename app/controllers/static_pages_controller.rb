57ad420c894ce0289f3adcc0ad7ef143466b3178
EC2 Default User <ec2-user@ip-172-31-39-103.us-east-2.compute.internal>
2018-11-19 19:27:10 +0000

    Created Static Pages and test file

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,2 +1,20 @@
 class StaticPagesController < ApplicationController
+  def home
+    
+  end
+
+  def info
+  end
+
+  def eshop
+  end
+
+  def login
+  end
+
+  def cart
+  end
+
+  def about
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,39 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+    test "should get root" do
+    get '/'
+    assert_response :success
+  end
+  
+  test "should get home" do
+    get static_pages_home_url
+    assert_response :success
+  end
+
+  test "should get info" do
+    get static_pages_info_url
+    assert_response :success
+  end
+
+  test "should get eshop" do
+    get static_pages_eshop_url
+    assert_response :success
+  end
+
+  test "should get login" do
+    get static_pages_login_url
+    assert_response :success
+  end
+
+  test "should get cart" do
+    get static_pages_cart_url
+    assert_response :success
+  end
+
+  test "should get about" do
+    get static_pages_about_url
+    assert_response :success
+  end
+
 end
